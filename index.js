c3a7e7e73359945062b9bf8987ed45e884b8e597
Rahmat Fauzi <madfizh@gmail.com>
2024-08-19 20:03:25 +0700

    Initial commit with login and register feature

---
+++ index.js
@@ -0,0 +1,38 @@
+// import express
+const express = require('express')
+
+// import cors
+const cors = require('cors')
+
+// import body-parser
+const bodyParser = require('body-parser')
+
+//import router
+const router = require('./routes')
+
+// init app
+const app = express();
+
+// use cors
+app.use(cors())
+
+// use body parser
+app.use(bodyParser.urlencoded({ extended:false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+//define port
+const port = 3000;
+
+// route
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+app.use('/api', router)
+
+// start server
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`)
+})
---
+++ routes/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+
+const { validateRegister, validateLogin } = require('../utils/validators/auth')
+const verifyToken = require('../middlewares/auth')
+
+const registerController = require('../controllers/RegisterController')
+const loginController = require('../controllers/LoginController')
+const userController = require('../controllers/UserController')
+
+router.post('/register', validateRegister, registerController.register)
+router.post('/login', validateLogin, loginController.login)
+
+router.get('/admin/users', verifyToken, userController.findUser)
+
+module.exports = router
