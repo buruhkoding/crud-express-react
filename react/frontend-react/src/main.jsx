dfa7687152bdc3c23ab3bb42f6878ff6b59da6b3
Rahmat Fauzi <madfizh@gmail.com>
2024-08-19 23:59:52 +0700

    Configure endpoint API Backend

--- react/frontend-react/src/main.jsx
+++ react/frontend-react/src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import { BrowserRouter } from 'react-router-dom'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
 )
